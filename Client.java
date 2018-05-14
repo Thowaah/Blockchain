607cf0bab94fa175d1a18c32cd7f098e509f2228
Thowaah <t.weissgerber@etu.unistra.fr>
2018-05-14 02:23:54 +0200

    debut d interface pour le client

--- Client.java
+++ Client.java
@@ -15,6 +15,9 @@ public class Client
 		{
 	    try
 			{
+        Scanner sc = new Scanner(System.in);
+
+        int quit = 0;
         DistributedBC bc = (DistributedBC) Naming.lookup("rmi://" + args[0] + "/DistributedBC");
 
         //System.out.println("Wesh");
@@ -37,7 +40,31 @@ public class Client
         String s = bc.ping();
         System.out.println(s);
 
+        do{
+          System.out.print(">");
+          String str = sc.nextLine();
+          String[] cmd = str.split(" ");
 
+          //System.out.println("entrée: " + cmd[0]);
+
+          if(cmd[0].equals("envoyer")) {
+            if(cmd.length == 3){
+              transactionVers(Integer.parseInt(cmd[1]), Float.parseFloat(cmd[2]), moi, bc);
+            } else {
+              System.out.println("usage: envoyer <id destinataire> <montant>");
+            }
+          } else if (cmd[0].equals("combien")) {
+            System.out.println("Vous possedez " +bc.combien(moi.getId()) + " points.");
+          } else if (cmd[0].equals("merite")) {
+            System.out.println("Votre merite est de " + bc.merite(moi.getId()) * 100 + "% de récompense du bloc");
+          } else {
+            System.out.println("commandes disponibles: envoyer, combien, merite");
+          }
+
+
+        } while(quit != 1);
+
+/*
         bc.payer(1,2,0.2f);
         System.out.println("Tr1 ok");
         TimeUnit.SECONDS.sleep(1);
@@ -61,24 +88,22 @@ public class Client
         System.out.println("P1= " + bc.combien(1));
         System.out.println("P1= " + bc.combien(2));
         System.out.println("P1= " + bc.combien(3));
+*/
 
-
-
-
-
-				//System.out.println("Le client recoit : ");
-				//int [][] res = mat.multiplicationMatrice(a,b);
-
-				/*		int o[] = {1,3,2};
-				System.out.println("egalite " +
-				Arrays.equals(res[0],o));
-				Arrays.sort(o);
-				System.out.println("egalite " +
-				Arrays.equals(res[0],o)); */
 	    }
 	    catch (NotBoundException re) { System.out.println(re) ; }
 	    catch (RemoteException re) { System.out.println(re) ; }
 	    catch (MalformedURLException e) { System.out.println(e) ; }
 		}
+  }
+
+  private static void transactionVers(int id, float montant, NoeudParticipant moi, DistributedBC bc)
+    throws RemoteException
+  {
+    if(bc.payer(moi.getId(), id, montant)){
+      System.out.println("Transaction envoyée");
+    } else {
+      System.out.println("Transaction échouée");
+    }
   }
 }
